396151fdb0ac7f2a8ce4d5c7d91b327a27f30b57
artem <awer2937@gmail.com>
2019-08-20 16:16:17 +0300

    Implement get Student all Teachers name page

--- src/main/java/com/volkov/UniversityCRUD/controller/StudentController.java
+++ src/main/java/com/volkov/UniversityCRUD/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.volkov.UniversityCRUD.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.volkov.UniversityCRUD.model.Student;
+import com.volkov.UniversityCRUD.model.Subject;
+import com.volkov.UniversityCRUD.model.Tutor;
 import com.volkov.UniversityCRUD.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
@@ -44,6 +50,23 @@ public class StudentController {
         student.ifPresent(student1 -> studentSubjectsCount.set(student1.getGroup().getSubjects().size()));
 
         return convertToJson(studentSubjectsCount);
+    }
+
+    @GetMapping("/student/getteachersname/{id}")
+    String getStudentTeachersName(@PathVariable @NotNull @DecimalMin("1") Long id) throws JsonProcessingException {
+        Optional<Student> student = studentsRepository.findById(id);
+
+        if (!student.isPresent())
+            return convertToJson(new ArrayList<String>());
+
+        AtomicReference<List<String>> teachers = new AtomicReference<>();
+        student.ifPresent(student1 -> teachers.set(student1.getGroup().getSubjects().stream()
+                .map(Subject::getTutor)
+                .filter(Objects::nonNull)
+                .map(Tutor::getFullName)
+                .collect(Collectors.toList())));
+
+        return convertToJson(teachers.get());
     }
 
     private String convertToJson(Object all) throws JsonProcessingException {
