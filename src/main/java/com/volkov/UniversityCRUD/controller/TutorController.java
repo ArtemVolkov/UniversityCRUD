9c476182250126df34eb4c513779801673e5d8a5
artem <awer2937@gmail.com>
2019-08-21 08:46:45 +0300

    Implement get Teacher Male Students page

---
+++ src/main/java/com/volkov/UniversityCRUD/controller/TutorController.java
@@ -0,0 +1,58 @@
+package com.volkov.UniversityCRUD.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.volkov.UniversityCRUD.model.Group;
+import com.volkov.UniversityCRUD.model.Student;
+import com.volkov.UniversityCRUD.model.Tutor;
+import com.volkov.UniversityCRUD.repository.TutorRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.volkov.UniversityCRUD.Util.JsonConverter.convertToJson;
+
+@RestController
+public class TutorController {
+
+    private final TutorRepository tutorRepository;
+
+    public TutorController(TutorRepository tutorRepository) {
+        this.tutorRepository = tutorRepository;
+    }
+
+    @GetMapping("/tutor/getstudentsbysex/{id}")
+    String getTutorMaleStudents(@PathVariable @NotNull @DecimalMin("1") Long id) throws JsonProcessingException {
+        Optional<Tutor> tutorOptional = tutorRepository.findById(id);
+
+        if (!tutorOptional.isPresent())
+            return convertToJson("Invalid tutor id");
+
+        List<Student> studentsMaleList = getTeacherGroups(tutorOptional.get()).stream()
+                .map(Group::getStudents)
+                .filter(Objects::nonNull)
+                .flatMap(List::stream)
+                .filter(student -> Objects.nonNull(student.getSex())
+                        && StringUtils.equalsIgnoreCase(student.getSex(), "Male"))
+                .collect(Collectors.toList());
+
+        return convertToJson(studentsMaleList);
+    }
+
+    private List<Group> getTeacherGroups(Tutor tutor) {
+        try {
+            return tutor.getSubject().getGroups();
+        } catch (NullPointerException e) {
+            return new ArrayList<>();
+        }
+
+    }
+}
